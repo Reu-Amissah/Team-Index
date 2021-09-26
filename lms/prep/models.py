60c40c2f5ae29f111b12ce5c5dcea60808936f27
Russell Quaicoo <russellquaicoo1@gmail.com>
2021-09-26 20:47:26 +0000

    Updated satic files and templates to django template language

--- lms/prep/models.py
+++ lms/prep/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class AdmissionForm():
+    passportpicture = models.ImageField(null=False, blank=False)
+    firstname = models.CharField(max_length=30, null=False)
+    lastname = models.CharField(max_length=20, null=False)
+    dob = models.DateField(null=False, blank=False)
+    
