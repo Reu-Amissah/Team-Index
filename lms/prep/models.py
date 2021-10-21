577e981c58f2d2bd7f0818882111a9e884afc50e
Russell Quaicoo <russellquaicoo1@gmail.com>
2021-10-21 07:37:02 +0000

    Created models for admission form

--- lms/prep/models.py
+++ lms/prep/models.py
@@ -8,4 +8,17 @@ class AdmissionForm():
     firstname = models.CharField(max_length=30, null=False)
     lastname = models.CharField(max_length=20, null=False)
     dob = models.DateField(null=False, blank=False)
+    gender = models.CharField(null=False, blank=False)
+    hometown = models.CharField(null=False, blank=False)
+    religion = models.Charfield(null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+
+    #parent details
+    parent_name = models.CharField(max_length=50, null=False, blank=False)
+    parent_occupation = models.CharField(max_length=20, null=False, blank=False)
+    parent_email = models.EmailField(blank=False, null=False)
+    address_line = models.CharField(null=False, blank=False)
+    number = models.CharField(max_length=10, null=False, blank=False)
+
+
     
