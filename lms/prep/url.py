0a991329c5ff653f96ecc8972d3b2c58bf2aaf66
Russell Quaicoo <russellquaicoo1@gmail.com>
2021-11-03 16:09:40 +0000

    new fields added to the database

--- lms/prep/url.py
+++ lms/prep/url.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.loginPage, name='loginPage'),
     path('contact/', views.contact, name='contact'),
     path('forum/', views.forum, name="forum"),
+    path('process_admission_form/', views.processAdmisssionForm,name="process_admission_form"),
+    path('process_contact_form/', views.processContact, name="process_contact")
 ]
--- lms/prep/views.py
+++ lms/prep/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse, request
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+import json
+from .models import *
 # Create your views here.
 
 def home(request):
@@ -29,6 +31,43 @@ def loginPage(request):
 def admissionForm(request):
     context = {}
     return render(request, 'admission.html', context)
+
+
+def processAdmisssionForm(request):
+    data = json.loads(request)
+    AdmissionForm.objects.create(
+        firstname = data['form']['firstname'],
+        lastname = data['form']['lastname'],
+        middlename = data['form']['middlename'],
+        dob = data['form']['dob'],
+        gender = data['form']['gender'],
+        hometown = data['form']['hometown'],
+        religion = data['form']['religion'],
+        email = data['form']['email'],
+        picture = data['form']['picture'],
+        disability = data['form']['disability'],
+        disability_info = data['form']['disability_info'],
+        transportation = data['form']['transportation'],
+
+        parent_name = data['form']['parent_name'],
+        parent_address_line = data['form']['parent_address_line'],
+        parent_occupation = data['form']['occupation'],
+        number = data['form']['contact'],
+        parent_email = data['form']['parent_email'],
+    )
+    return JsonResponse("Form sent", safe=False)
+
+def processContact(request):
+    data = json.loads(request)
+    Contact.objects.create(
+        firstname = data['form']['firstname'],
+        lastname = data['form']['lastname'],
+        email = data['form']['email'],
+        phone = data['form']['phone'],
+        message = data['form']['message']
+    )
+    return JsonResponse("form has been sent", safe=False)
+
 
 def forum(request):
     context = {}
